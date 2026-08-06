e8aa24f741cc441e690863f9642b73595c5d9e57
swastikttpvtltd <swastikttpvtltd@gmail.com>
2026-08-07 00:18:55 +0530

    Add best travel time to every package

--- data/packages.ts
+++ data/packages.ts
@@ -21,6 +21,7 @@ import islandPackages from "./packages/island-packages";
 import { defaultPackageExclusions } from "./defaultPackageExclusions";
 import { defaultPackageInclusions } from "./defaultPackageInclusions";
 import { makePackageRates } from "./packagePricing";
+import { getBestTime } from "./packageBestTime";
 
 const rawPackages=[...islandPackages,...nextStatesPackages,...ladakhPackages,...himachalPackages,...kashmirPackages,...newMultiStatePackages,...uttarPradeshPackages,...uttarakhandPackages,rajasthanGrandTour,jaipurJodhpurUdaipur,jodhpurJaisalmer,udaipurMountAbu,jaipurAjmerPushkar,jaipurHeritage,gujaratGrandTour,dwarkaSomnath,girNationalPark,ramUtsav,saputara,statueOfUnity];
 const standardHotels=[{name:"3-Star Hotel / Similar",category:"3-Star",star:"3-Star Hotel"}];
@@ -34,6 +35,6 @@ const makePackageId=(id:unknown,slug?:unknown,title?:unknown)=>{
 
 export const packages=rawPackages.map((pkg)=>{
   const groupRates=makePackageRates(pkg);
-  return {...pkg,packageId:makePackageId(pkg.id,pkg.slug,pkg.title),price:groupRates[6],groupRates,priceBasis:"Per Person | 3-Star Hotel / Similar | Breakfast & Dinner | Standard Transport & Sightseeing",hotels:standardHotels.map((hotel)=>({...hotel})),meals:[...standardMeals],exclusions:[...defaultPackageExclusions,"Lunch and any meals other than the included breakfast and dinner"],inclusions:[...defaultPackageInclusions,"Accommodation in 3-Star Hotels / Similar","Breakfast and Dinner at hotel; buffet service subject to hotel policy and occupancy"]};
+  return {...pkg,packageId:makePackageId(pkg.id,pkg.slug,pkg.title),price:groupRates[6],groupRates,bestTime:getBestTime(pkg),bestTimeToVisit:getBestTime(pkg),priceBasis:"Per Person | 3-Star Hotel / Similar | Breakfast & Dinner | Standard Transport & Sightseeing",hotels:standardHotels.map((hotel)=>({...hotel})),meals:[...standardMeals],exclusions:[...defaultPackageExclusions,"Lunch and any meals other than the included breakfast and dinner"],inclusions:[...defaultPackageInclusions,"Accommodation in 3-Star Hotels / Similar","Breakfast and Dinner at hotel; buffet service subject to hotel policy and occupancy"]};
 });
 export default packages;
