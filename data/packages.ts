6cb6d3f720d1b3cec7e67cd6d827bc2d889313fc
swastikttpvtltd <swastikttpvtltd@gmail.com>
2026-08-06 20:34:05 +0530

    Register Uttar Pradesh tour and corporate packages

--- data/packages.ts
+++ data/packages.ts
@@ -11,10 +11,12 @@ import jaipurAjmerPushkar from "./packages/rajasthan/jaipur-ajmer-pushkar";
 import jaipurJodhpurUdaipur from "./packages/rajasthan/jaipur-jodhpur-udaipur";
 import rajasthanGrandTour from "./packages/rajasthan/rajasthan-grand-tour";
 import uttarakhandPackages from "./packages/uttarakhand/uttarakhand-packages";
+import uttarPradeshPackages from "./packages/uttar-pradesh/uttar-pradesh-packages";
 import { defaultPackageExclusions } from "./defaultPackageExclusions";
 import { defaultPackageInclusions } from "./defaultPackageInclusions";
 
 const rawPackages = [
+  ...uttarPradeshPackages,
   ...uttarakhandPackages,
   rajasthanGrandTour, jaipurJodhpurUdaipur, jodhpurJaisalmer, udaipurMountAbu, jaipurAjmerPushkar, jaipurHeritage,
   gujaratGrandTour, dwarkaSomnath, girNationalPark, ramUtsav, saputara, statueOfUnity,
