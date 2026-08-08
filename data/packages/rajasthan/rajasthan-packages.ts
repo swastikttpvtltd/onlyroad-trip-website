7372a01e4e39874bfd8b4fb566038b9459409010
swastikttpvtltd <swastikttpvtltd@gmail.com>
2026-08-08 17:29:28 +0530

    Add Rajasthan state package index

---
+++ data/packages/rajasthan/rajasthan-packages.ts
@@ -0,0 +1,17 @@
+import jaipurHeritage from "./jaipur-heritage";
+import jodhpurJaisalmer from "./jodhpur-jaisalmer";
+import udaipurMountAbu from "./udaipur-mount-abu";
+import jaipurAjmerPushkar from "./jaipur-ajmer-pushkar";
+import jaipurJodhpurUdaipur from "./jaipur-jodhpur-udaipur";
+import rajasthanGrandTour from "./rajasthan-grand-tour";
+
+const rajasthanPackages = [
+  jaipurHeritage,
+  jodhpurJaisalmer,
+  udaipurMountAbu,
+  jaipurAjmerPushkar,
+  jaipurJodhpurUdaipur,
+  rajasthanGrandTour,
+];
+
+export default rajasthanPackages;
