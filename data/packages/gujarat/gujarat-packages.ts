2b8e11f670650c65a811463e91b7aafc6734ee47
swastikttpvtltd <swastikttpvtltd@gmail.com>
2026-08-08 17:29:36 +0530

    Add Gujarat state package index

---
+++ data/packages/gujarat/gujarat-packages.ts
@@ -0,0 +1,17 @@
+import dwarkaSomnath from "./dwarka-somnath";
+import girNationalPark from "./gir-national-park";
+import rannUtsav from "./rann-utsav";
+import saputara from "./saputara";
+import statueOfUnity from "./statue-of-unity";
+import gujaratGrandTour from "./gujarat-grand-tour";
+
+const gujaratPackages = [
+  dwarkaSomnath,
+  girNationalPark,
+  rannUtsav,
+  saputara,
+  statueOfUnity,
+  gujaratGrandTour,
+];
+
+export default gujaratPackages;
