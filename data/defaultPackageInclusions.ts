078f7ff703cd5d955449b5b71487fdffdcb5e05e
swastikttpvtltd <swastikttpvtltd@gmail.com>
2026-08-10 23:17:21 +0530

    Remove travel insurance from default package inclusions

--- data/defaultPackageInclusions.ts
+++ data/defaultPackageInclusions.ts
@@ -5,7 +5,6 @@ export const defaultPackageInclusions = [
   "All entrance fees of the sightseeing places mentioned in the itinerary.",
   "Services of the Tour Manager from the first day of meeting point to the last day dropping point of the tour. (Except for Uttaranchal Chardham Yatra).",
   "Cost of any internal airfare if specified in the itinerary.",
-  "Cost of Travel insurance up to 70 years of age for India Tours and World Tours (Except for Kailash Mansarovar Yatra).",
   "Driver Tips and Guide Tips as per the itinerary.",
   "City Tax (if applicable) as per the itinerary.",
 ];
