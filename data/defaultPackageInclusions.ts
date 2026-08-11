d53eec9d9680f2745607a3e1021fe12d337e6684
swastikttpvtltd <swastikttpvtltd@gmail.com>
2026-08-12 00:45:19 +0530

    Update default transport inclusion wording

--- data/defaultPackageInclusions.ts
+++ data/defaultPackageInclusions.ts
@@ -1,7 +1,7 @@
 export const defaultPackageInclusions = [
   "Accommodation on twin sharing basis for the duration of the tour in the itinerary.",
   "Meals as mentioned in the itinerary.",
-  "Coach transfers from first day meeting point of the group to the last day of the dropping point of the group of the tour.",
+  "Complete transfers and daily sightseeing by a private air-conditioned vehicle. The vehicle type (Sedan / SUV / Tempo Traveller / Coach) will be strictly allocated based on the final group size.",
   "All entrance fees of the sightseeing places mentioned in the itinerary.",
   "Services of the Tour Manager from the first day of meeting point to the last day dropping point of the tour. (Except for Uttaranchal Chardham Yatra).",
   "Cost of any internal airfare if specified in the itinerary.",
