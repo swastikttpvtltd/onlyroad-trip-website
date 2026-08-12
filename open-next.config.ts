c9fe6a8d139dde2a08bdb07c163b1488ab47b9a4
swastikttpvtltd <swastikttpvtltd@gmail.com>
2026-08-12 18:29:51 +0530

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,7 +3,10 @@ const config = {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
+      proxyExternalRequest: "fetch",
       incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 };
