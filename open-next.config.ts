4fc3ece6e02335f79ca76bc896ce396be40c0ef5
swastikttpvtltd <swastikttpvtltd@gmail.com>
2026-08-12 18:34:03 +0530

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -9,6 +9,17 @@ const config = {
       queue: "dummy",
     },
   },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
 };
 
 export default config;
