f2e31dea3b746b7b78464c2c6a3e59ddc88f8ae7
swastikttpvtltd <swastikttpvtltd@gmail.com>
2026-08-20 12:23:14 +0530

    feat: restore Jyotirlinga experience theme

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,7 @@ import { usePathname, useRouter } from "next/navigation";
 const experienceThemes = [
   { name: "Spiritual", query: "Spiritual", description: "Sacred journeys & peaceful escapes" },
   { name: "Pilgrimage", query: "Pilgrimage", description: "Temples, dhams & holy circuits" },
+  { name: "Jyotirlinga", query: "Jyotirlinga", description: "Sacred Jyotirlinga temple journeys" },
   { name: "Trekking", query: "Trekking", description: "Trails, peaks & Himalayan walks" },
   { name: "Adventure", query: "Adventure", description: "Thrilling outdoor journeys" },
   { name: "Women Special", query: "Women", description: "Curated journeys for women" },
