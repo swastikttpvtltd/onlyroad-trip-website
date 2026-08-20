ee56d6a4d839733f79b699b21f6fc9954d0d831a
swastikttpvtltd <swastikttpvtltd@gmail.com>
2026-08-20 13:03:44 +0530

    feat: merge individual Jyotirlinga packages into library

--- data/packages/gujarat/jyotirlinga-packages.ts
+++ data/packages/gujarat/jyotirlinga-packages.ts
@@ -1,4 +1,5 @@
 import library from "../multi-state/jyotirlinga-packages";
+import individualJyotirlingaPackages from "../multi-state/individual-jyotirlinga-packages";
 
-export const jyotirlingaPackages = library;
+export const jyotirlingaPackages = [...library, ...individualJyotirlingaPackages];
 export default jyotirlingaPackages;
