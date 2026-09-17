31db83a2c667fadc024f44ca037add70d4a4aab4
swastikttpvtltd <swastikttpvtltd@gmail.com>
2026-09-18 00:48:42 +0530

    Optimize package hero image sizing

--- components/package/PackageHero.tsx
+++ components/package/PackageHero.tsx
@@ -21,6 +21,7 @@ export default function PackageHero({ data }: PackageHeroProps) {
         alt={data.title}
         fill
         priority
+        sizes="100vw"
         className="object-cover"
       />
 
